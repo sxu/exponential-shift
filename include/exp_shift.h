6b520cd8af6b4820c021150c89b992abf9b286df
Shen Chen Xu <shenchenxu@gmail.com>
2017-03-11 01:54:23 -0500

    Clustering via BFS and generating exponentials via order statistics

--- include/exp_shift.h
+++ include/exp_shift.h
@@ -1,6 +1,7 @@
 #ifndef EXP_SHIFT_H
 #define EXP_SHIFT_H
 
+#include <algorithm>
 #include <random>
 #include <vector>
 #include "graph.h"
@@ -13,20 +14,11 @@ void add_exponential_shifts(std::vector<VtxT>& vs,
                             PairingHeap<VtxT, CmpT, Offset>& queue,
                             double lambda) {
   size_t n = vs.size();
-  std::vector<double> shifts(n);
-  double max_shift = -1;
   std::mt19937 rng(std::random_device{}());
   std::exponential_distribution<double> exp_dist(lambda);
 
   for (size_t i = 0; i < n; i++) {
-    shifts[i] = exp_dist(rng);
-    if (shifts[i] > max_shift) {
-      max_shift = shifts[i];
-    }
-  }
-
-  for (size_t i = 0; i < n; i++) {
-    vs[i].initialize(i, max_shift - shifts[i] + 1);
+    vs[i].initialize(i, -exp_dist(rng));
     queue.insert(vs[i]);
   }
 }
@@ -44,6 +36,52 @@ void dijkstra(const Graph<AdjT>& g,
       if (vs[a.v].relax(u, a)) {
         queue.decrease_key(vs[a.v]);
       }
+    }
+  }
+}
+
+template <typename VtxT>
+void add_exponential_shifts(std::vector<VtxT>& vs,
+                            std::vector<size_t>& q,
+                            double lambda) {
+  std::mt19937 rng(std::random_device{}());
+  std::uniform_real_distribution<double> unif(0, 1);
+  size_t n = vs.size();
+
+  std::shuffle(q.begin(), q.end(), rng);
+  double sum = 0;
+  for (size_t i = q.size(); i > 0; i--) {
+    sum += std::log(1 - unif(rng)) / (lambda * i);
+    vs[q[i - 1]].initialize(q[i - 1], sum);
+  }
+}
+
+template <typename AdjT, typename VtxT, typename CmpT>
+void bfs(const Graph<AdjT>& g,
+         std::vector<VtxT>& vs,
+         std::vector<size_t>& q1,
+         std::vector<size_t>& q2,
+         CmpT& less) {
+  size_t i1 = 0;
+  size_t i2 = 0;
+  for (;;) {
+    size_t u;
+    if (i1 < q1.size() && i2 < q2.size()) {
+      if (less(vs[q1[i1]], vs[q2[i2]])) {
+        u = q1[i1++];
+      } else {
+        u = q2[i2++];
+      }
+    } else if (i1 < q1.size()) {
+      u = q1[i1++];
+    } else if (i2 < q2.size()) {
+      u = q2[i2++];
+    } else {
+      break;
+    }
+    for (size_t i = g.first_adj[u]; i < g.first_adj[u + 1]; i++) {
+      const AdjT& a = g.adjacencies[i];
+      vs[a.v].relax(u, a);
     }
   }
 }
